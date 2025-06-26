14fa77cac80df85c3dbf21edbe57b93eddef9467
Thanhhuy2000 <147130351+Thanhhuy2000@users.noreply.github.com>
2025-06-26 21:06:29 +0700

    up

---
+++ dong_goi/lop.java
@@ -0,0 +1,6 @@
+public class lop {
+    public static void main(String[] args){
+        hoc_sinh str = new hoc_sinh("Huy", 24);
+        System.out.println(str.getName("thanh"));
+    }
+}
---
+++ ke_thua/P1/Pk.java
@@ -0,0 +1,8 @@
+package ke_thua.P1;
+
+public class Pk {
+    public int a;
+    int b;
+    protected int c;
+    private int d;
+}
